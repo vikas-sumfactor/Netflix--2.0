15978bc7e08f05bf864df6d0a591ff5d48852775
vikas-sumfactor <vikas.sumfactor@gmail.com>
2023-05-30 17:21:34 +0530

    graphql and hasura implementation

---
+++ lib/db/hasura.tsx
@@ -0,0 +1,43 @@
+async function queryHasuraGQL(operationsDoc:any, operationName:any, variables:any) {
+
+    const url:any = process.env.NEXT_PUBLIC_HASURA_ADMIN_URL;
+
+    const result = await fetch(url, {
+      method: "POST",
+      headers: {
+        "x-hasura-admin-secret": process.env.NEXT_PUBLIC_HASURA_ADMIN_SECRET,
+      },
+      body: JSON.stringify({
+        query: operationsDoc,
+        variables: variables,
+        operationName: operationName,
+      }),
+    });
+    return await result.json();
+  }
+  
+  function fetchMyQuery() {
+    const operationsDoc = `
+      query MyQuery {
+        users {
+          email
+          id
+          issuer
+          publicAddress
+        }
+      }
+    `;
+    return queryHasuraGQL(operationsDoc, "MyQuery", {});
+  }
+  
+  export async function startFetchMyQuery() {
+    
+    const { errors, data } = await fetchMyQuery();
+    if (errors) {
+      // handle those errors like a pro
+      console.error(errors);
+    }
+    // do something great with this precious data
+    console.log(data);
+  }
+  startFetchMyQuery();
